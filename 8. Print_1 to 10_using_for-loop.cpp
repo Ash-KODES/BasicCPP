7d701fe546bfa1a5ecc44d51e7e96972ba3a760d
Akash Prasher <42891970+akashprasher@users.noreply.github.com>
2019-04-11 21:23:45 +0530

    Using for-loop for print numbers 1-10

--- 8. Print_1 to 10_using_for-loop.cpp
+++ 8. Print_1 to 10_using_for-loop.cpp
@@ -10,6 +10,12 @@ using namespace std;
 
 int main()
 {
+    for(int i=0;i<10;i++) 
+    {
+        cout<<i+1<<endl;
+    }
+}
+
 /*
     In this program we'll use for-loop for print numbers 1-10
     which means we don't required to write output again and again.
@@ -24,8 +30,3 @@ int main()
     part3--> increment/decrement adding "++" with integer will increase it's value by one after every cycle
     whereas adding "--" will subtract 1 from integer.
 */
-    for(int i=0;i<10;i++) 
-    {
-        cout<<i+1<<endl;
-    }
-}
