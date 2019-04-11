1443644d6922abe80d428996d8b58d6cd9c802cd
Akash Prasher <42891970+akashprasher@users.noreply.github.com>
2019-04-11 21:19:50 +0530

    Print Number Using For-Loop with Explaination.

---
+++ 8. Print_1 to 10_using_for-loop.cpp
@@ -0,0 +1,31 @@
+/*
+    PRINTING NUMBERS 1 TO 10 USING FOR-LOOP
+    ***********************************************************
+    @author : Akash Prasher ~~ https://github.com/akashprasher/
+    This Code is written in CodeBlocks.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+/*
+    In this program we'll use for-loop for print numbers 1-10
+    which means we don't required to write output again and again.
+    for-loop will automatically do it.
+*/
+/*
+    For-Loop
+    It consist of three parts
+    for(part1;part2;part3)
+    part1--> integer value which points to initial value of loop
+    part2--> integer value which works as the limitation or end point of loop
+    part3--> increment/decrement adding "++" with integer will increase it's value by one after every cycle
+    whereas adding "--" will subtract 1 from integer.
+*/
+    for(int i=0;i<10;i++) 
+    {
+        cout<<i+1<<endl;
+    }
+}
