274d379c31411a9402e9a1dd2aa8f92fb6c339bf
Akash Prasher <42891970+akashprasher@users.noreply.github.com>
2019-04-11 19:23:48 +0530

    Update 6. Calculator_using_Function.cpp

--- 6. Calculator_using_Function.cpp
+++ 6. Calculator_using_Function.cpp
@@ -43,11 +43,13 @@ int division (int a, int b)
 int main ()
 {
   int num1,num2,choice,res;
+
   //Getting input
   cout<<"Enter the First Number"<<endl;
   cin>>num1;
   cout<<"Enter the Second Number"<<endl;
   cin>>num2;
+   
   //Menu with option for operations
   cout<<"\nChoose Any ONE(1-4)"<<endl;
   cout<<"1. Addition"<<endl;
@@ -60,30 +62,35 @@ int main ()
 /**checking the entered option using
 if/else statement
 */
+    
   //if addition is chosen.
   if(choice==1)
   {
       res=addition(num1,num2);
       cout<<"Addition : "<<res<<endl;
   }
+    
   //if subtraction is chosen.
   else if(choice==2)
   {
       res=sub(num1,num2);
       cout<<"Subtraction : "<<res<<endl;
   }
+    
   //if multiplication is chosen
   else if(choice==3)
   {
       res=multiply(num1,num2);
       cout<<"Multiplication : "<<res<<endl;
   }
+    
   //if division is chosen
   else if(choice==4)
   {
       res=division(num1,num2);
       cout<<"Division : "<<res<<endl;
   }
+    
   //for wrong input (i.e. except 1-4)
   else
   {
