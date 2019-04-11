87313534b7dcdb7ed2fad974ce9b59b1221ae3ed
Akash Prasher <42891970+akashprasher@users.noreply.github.com>
2019-04-11 18:26:28 +0530

    Create 5. Multiplication_of_two_numbers.cpp

---
+++ 5. Multiplication_of_two_numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream> //Header File
+using namespace std; //For Input(cin) and Output(cout)
+
+int main() //main function
+{
+    int a,b,mul; //declaring variables of integer type
+    cout<<"Enter the first number."<<endl; //asking for entering first number
+    cin>>a; //getting input for first number
+    cout<<"Enter the second number."<<endl; //asking for entering second number
+    cin>>b; //getting input for second number
+
+    mul=a*b; //multiply the numbers and storing in another variable.
+
+    cout<<"Multiplication : "<<mul<<endl; //showing multiplications of both numbers
+}
